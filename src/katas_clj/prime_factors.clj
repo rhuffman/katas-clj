2d501f5fc6882ff5258ee10889ce91d42d037e7e
Robert Huffman <robert.huffman@gmail.com>
2014-09-14 13:30:34 -0700

    Implementing prime-factors

--- src/katas_clj/prime_factors.clj
+++ src/katas_clj/prime_factors.clj
@@ -1,5 +1,10 @@
 (ns katas-clj.prime-factors)
 
-(defn prime-factors-1
-  [n]
-  (list n))
+(defn prime-factors-1 [n]
+  (assert (> n 0))
+  (let [primes []]
+    (cond
+      (= n 1) [1]
+      (= n 2) [2]
+      (> n 2) (if (even? n) (conj primes 2 (/ n 2)) (conj primes n))
+      :else primes)))
