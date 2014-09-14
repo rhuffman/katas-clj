e584a0b005fe9171730e355815535b6ab16368b5
Robert Huffman <robert.huffman@gmail.com>
2014-09-14 13:02:01 -0700

    Refactored prime-factors test so test-data is a sequence instead of a map

--- test/katas_clj/prime_factors_test.clj
+++ test/katas_clj/prime_factors_test.clj
@@ -4,22 +4,22 @@
 
 ; Map of test data. Key is input to the function being tested;
 ; value is ; the expected output
-(def test-data {1 [1]
-                2 [2]
-                3 [3]
-                4 [2 2]
-                5 [5]
-                6 [2 3]
-                7 [7]
-                8 [2 2 2]
-                9 [3 3]
-                10 [2 5]
-                11 [11]
-                12 [2 2 3]
-                13 [13]
-                14 [2 7]
-                15 [3 5]
-                16 [2 2 2 2]})
+(def test-data [[1 [1]]
+                [2 [2]]
+                [3 [3]]
+                [4 [2 2]]
+                [5 [5]]
+                [6 [2 3]]
+                [7 [7]]
+                [8 [2 2 2]]
+                [9 [3 3]]
+                [10 [2 5]]
+                [11 [11]]
+                [12 [2 2 3]]
+                [13 [13]]
+                [14 [2 7]]
+                [15 [3 5]]
+                [16 [2 2 2 2]]])
 
 ; Array of functions to test
 (def test-functions [prime-factors-1])
@@ -29,7 +29,8 @@
 ; expected value in test-data
 (defn run [test-function]
   (doseq [[input expected] test-data]
-    (is (= (test-function input) expected))))
+    (let [actual (test-function input)]
+          (is (= actual expected)))))
 
 ; Iterate the list of functions, testing each in turn
 (deftest all-functions-test
