dc701c9a7115bc0099e7f87a9ace5be57cf938be
Robert Huffman <robert.huffman@gmail.com>
2014-09-15 17:47:13 -0700

    Cleaned up the internal function a bit

--- src/katas_clj/prime_factors.clj
+++ src/katas_clj/prime_factors.clj
@@ -1,11 +1,9 @@
 (ns katas-clj.prime-factors)
 
-(defn prime-factors-for-even [n]
-  (let [primes []]
-    (cond
-      (and (odd? n) (> n 1)) [n]
-      (odd? n) []
-      :else (cons 2 (prime-factors-for-even (/ n 2))))))
+(defn prime-factors [n]
+    (if (odd? n)
+      (if (> n 1) [n] [])
+      (cons 2 (prime-factors (/ n 2)))))
 
 (defn prime-factors-1 [n]
   (assert (> n 0))
@@ -14,5 +12,5 @@
       (= n 1) [1]
       (= n 2) [2]
       (odd? n) [n]
-      :else (prime-factors-for-even n))))
+      :else (prime-factors n))))
 
