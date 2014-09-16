81f47027a9cbca1a0d8c58f296f2f5b72e5557a6
Robert Huffman <robert.huffman@gmail.com>
2014-09-15 17:17:35 -0700

    Passes, but this cannot possibly be idiomatic Clojure

--- src/katas_clj/prime_factors.clj
+++ src/katas_clj/prime_factors.clj
@@ -1,10 +1,18 @@
 (ns katas-clj.prime-factors)
 
+(defn prime-factors-for-even [n]
+  (let [primes []]
+    (cond
+      (and (odd? n) (> n 1)) [n]
+      (odd? n) []
+      :else (cons 2 (prime-factors-for-even (/ n 2))))))
+
 (defn prime-factors-1 [n]
   (assert (> n 0))
   (let [primes []]
     (cond
       (= n 1) [1]
       (= n 2) [2]
-      (> n 2) (if (even? n) (conj primes 2 (/ n 2)) (conj primes n))
-      :else primes)))
+      (odd? n) [n]
+      :else (prime-factors-for-even n))))
+
