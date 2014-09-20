a6b55e923138ded46039b1e84b31139561a605a3
Robert Huffman <robert.huffman@gmail.com>
2014-09-19 17:34:02 -0700

    Add more tests of simple cases

--- src/katas_clj/roman_numerals.clj
+++ src/katas_clj/roman_numerals.clj
@@ -3,9 +3,14 @@
 (defn divisible-by? [n divisor]
   (zero? (rem n divisor)))
 
-(def symbols [{ :value 10 :symbol "X"}
-              { :value 5  :symbol "V"}
-              { :value 1  :symbol "I"}
+(def symbols [
+               { :value 1000 :symbol "M"}
+               { :value 500  :symbol "D"}
+               { :value 100  :symbol "C"}
+               { :value 50   :symbol "L"}
+               { :value 10   :symbol "X"}
+               { :value 5    :symbol "V"}
+               { :value 1    :symbol "I"}
               ])
 
 (defn next-symbol [n]
