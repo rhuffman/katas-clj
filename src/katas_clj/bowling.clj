051efc33002b77525693162fcff34825fc95f356
Robert Huffman <robert.huffman@gmail.com>
2014-09-22 08:41:39 -0700

    A bit of refactoring

--- src/katas_clj/bowling.clj
+++ src/katas_clj/bowling.clj
@@ -19,11 +19,9 @@
     ))
 
 (defn frame-total
-  "Given the total before a frame, the first roll for the frame and the next two rolls,
+  "Given the total before a frame, the first roll for the frame and the two rolls after that,
   returns the total after the frame."
   [total three-rolls]
-
-  (assert (= 3 (.length three-rolls)))
 
   (let [roll-1 (get three-rolls 0)
         roll-2 (get three-rolls 1)
@@ -33,10 +31,11 @@
       (= 10 (+ roll-1 roll-2)) (+ total roll-1 roll-2 roll-3)
       :else (+ total (reduce + (take 2 three-rolls))))))
 
-(defn frame [total three-rolls]
-  [(get three-rolls 0)
-   (if (= 10 (get three-rolls 0)) nil (get three-rolls 1))
-   (frame-total total three-rolls)])
+(defn frame [total pin-counts]
+  (let [[roll-1 roll-2] (take 2 pin-counts)]
+    [roll-1                               ; set first roll of frame to roll-1
+     (if (= 10 roll-1) nil roll-2)        ; set second roll: if strike, set to nil; otherwise, set to roll-2
+     (frame-total total pin-counts)]))    ; set cumulative total
 
 ;; Takes a sequence of pin counts thrown on successive
 ;; rolls by a single player during a game of bowling.
