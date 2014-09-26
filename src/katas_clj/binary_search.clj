43502562ac652c2d0fa29e4c9bac23221ebf4f32
Robert Huffman <robert.huffman@gmail.com>
2014-09-26 06:12:02 -0700

    New failing test

--- src/katas_clj/binary_search.clj
+++ src/katas_clj/binary_search.clj
@@ -1,16 +1,16 @@
 (ns katas-clj.binary-search)
 
 (defn search
-  ([vec value]
-  (search vec value 0))
-  ([vec value start-index]
-  (cond
-    (= 0 (.length vec)) nil
-    (= 1 (.length vec)) (if (= value (get vec 0)) start-index nil)
-    :else
-    (let [half (/ (.length vec) 2)
-          vec-value (get vec half) ]
-      (cond
-        (= value vec-value) (+ start-index half)
-        (< value vec-value) (search (subvec vec 0 half) value start-index)
-        (> value vec-value) (search (subvec vec half) value (+ start-index half)))))))
+  ([v value]
+  (search v value 0))
+  ([v value start-index]
+   (cond
+     (= 0 (.length v)) nil
+     (= 1 (.length v)) (if (= value (get v 0)) start-index nil)
+     :else
+     (let [half (/ (.length v) 2)
+           vec-value (get v half) ]
+       (cond
+         (= value vec-value) (+ start-index half)
+         (< value vec-value) (search (subvec v 0 half) value start-index)
+         (> value vec-value) (search (subvec v half) value (+ start-index half)))))))
--- test/katas_clj/binary_search_test.clj
+++ test/katas_clj/binary_search_test.clj
@@ -3,14 +3,26 @@
             [clojure.test.tap :refer :all]
             [katas-clj.binary-search :refer :all]))
 
-(deftest test-search
-  (are [vec value expected] (= expected (search vec value))
-                            [] 100 nil
-                            [100] 100 0
-                            [100 101] 100 0
-                            [100 101] 101 1
-                            [100 101 102 103] 102 2
-                            [100 101 102 104] 104 3
-  ))
+;(deftest test-search
+;  (are [vec value expected] (= expected (search vec value))
+;                            [] 100 nil
+;                            [100] 100 0
+;                            [100 101] 100 0
+;                            [100 101] 101 1
+;                            [100 101 102 103] 102 2
+;                            [100 101 102 104] 104 3
+;                            ))
+
+(defn add-random [x]
+  (+ x (int (* 10 (rand)))))
+
+(deftest test-big-vector
+  (let [vector (vec (take 5000 (iterate add-random 1)))
+        size (.length vector)
+        idx 4323
+        value (get vector idx)
+        actual (search vector value)]
+    (is (= idx actual))))
+
 
 (run-tests)
