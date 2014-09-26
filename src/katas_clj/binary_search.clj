629ec32a268ad7c42ebca4f9775418b2632ee38d
Robert Huffman <robert.huffman@gmail.com>
2014-09-25 20:06:48 -0700

    Working binary search

--- src/katas_clj/binary_search.clj
+++ src/katas_clj/binary_search.clj
@@ -7,4 +7,10 @@
   (cond
     (= 0 (.length vec)) nil
     (= 1 (.length vec)) (if (= value (get vec 0)) start-index nil)
-    )))
+    :else
+    (let [half (/ (.length vec) 2)
+          vec-value (get vec half) ]
+      (cond
+        (= value vec-value) (+ start-index half)
+        (< value vec-value) (search (subvec vec 0 half) value start-index)
+        (> value vec-value) (search (subvec vec half) value (+ start-index half)))))))
